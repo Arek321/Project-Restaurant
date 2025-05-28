92e81a996176238d0e2bfdc8cf9bdf95518efa66
Arkadiusz <paniqq27@gmail.com>
2025-05-28 02:42:48 +0200

    Added first method to UserController with proper annotations and Swagger UI, tested the method

--- RestaurantMS/src/main/java/org/example/restaurantms/controller/UserController.java
+++ RestaurantMS/src/main/java/org/example/restaurantms/controller/UserController.java
@@ -1,4 +1,27 @@
 package org.example.restaurantms.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.example.restaurantms.entity.User;
+import org.example.restaurantms.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@Tag(name="User")
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Operation(summary = "Retrieve all Users", description = "Returns a list of all Users")
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
