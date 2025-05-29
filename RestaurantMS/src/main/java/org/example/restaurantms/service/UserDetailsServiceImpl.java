8e978e1a2c320aa66d47e69c8e0816c3093409f3
Arkadiusz <paniqq27@gmail.com>
2025-05-29 21:21:25 +0200

    Added Spring Security depencency and created basic authentication

---
+++ RestaurantMS/src/main/java/org/example/restaurantms/service/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package org.example.restaurantms.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.restaurantms.entity.User;
+import org.example.restaurantms.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Użytkownik nie istnieje: " + username));
+
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().name());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(authority)
+        );
+    }
+}
